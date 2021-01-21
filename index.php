2cf575abd4bc4177ff88dbeab0b0860a8bd21e9e
jpbondilles <77731822+jpbondilles@users.noreply.github.com>
2021-01-21 12:02:44 +0800

    Update index.php
    Updated the way of using queries with stored procedures.

--- index.php
+++ index.php
@@ -186,160 +186,147 @@
 
 	<body>
 
-			<form action="index.php" method="get">
+		<form action="index.php" method="get">
 
-				<?php
+		<?php
 
-						if(isset($_GET['delUser'])){
+			if(isset($_GET['delUser'])){
 
-							$userID = $_GET['delUser'];
+				$userID = $_GET['delUser'];
 
-							$sql = "CALL deleteData(:userID)";
-							$cmd = $conn->prepare($sql);
-							$cmd->bindParam("userID",$userID);
-							$cmd->execute();
+				$sql = "CALL deleteData(:userID)";
+				$cmd = $conn->prepare($sql);
+				$cmd->bindParam("userID",$userID);
+				$cmd->execute();
 
-						} //this block of codes enables the user to delete a data from the database.
+			} //this block of codes enables the user to delete a data from the database.
 
 
-								if (isset($_GET['btnSave'])) {
+			if (isset($_GET['btnSave'])) {
 			
 
-										$firstName = $_GET['txtFname'];
-										$middleName = $_GET['txtMname'];
-										$lastName = $_GET['txtLname'];
-										$phoneNumber = $_GET['txtPhoneNumber'];
-										$email = $_GET['txtEmail'];
+				$firstName = $_GET['txtFname'];
+				$middleName = $_GET['txtMname'];
+				$lastName = $_GET['txtLname'];
+				$phoneNumber = $_GET['txtPhoneNumber'];
+				$email = $_GET['txtEmail'];
 
 
-										$userID = $_SESSION['userID'];
-										$checker = $_SESSION['checker'];
+				$userID = $_SESSION['userID'];
+				$checker = $_SESSION['checker'];
 
-										if (empty($checker)) {
-	
-											$sql = "CALL saveData(:firstName, :middleName, :lastName, :phoneNumber, :emailAddress)";
+				if (empty($checker)) {
 
-											try {
+					$sql = "CALL saveData(:firstName, :middleName, :lastName, :phoneNumber, :emailAddress)";
 
-													
-												$cmd = $conn->prepare($sql);
-												$cmd->bindParam("firstName", $firstName);
-												$cmd->bindParam("middleName", $middleName);
-												$cmd->bindParam("lastName", $lastName);
-												$cmd->bindParam("phoneNumber", $phoneNumber);
-												$cmd->bindParam("emailAddress", $email);
-												$cmd->execute();	
-													
-												} catch (Exception $e) {
+					try {					
+						$cmd = $conn->prepare($sql);
+						$cmd->bindParam("firstName", $firstName);
+						$cmd->bindParam("middleName", $middleName);
+						$cmd->bindParam("lastName", $lastName);
+						$cmd->bindParam("phoneNumber", $phoneNumber);
+						$cmd->bindParam("emailAddress", $email);
+						$cmd->execute();	
 
-													echo  $e->getMessage();
+					} catch (Exception $e) {
 
-												}
+						echo  $e->getMessage();
 
-												$saveChecker = 1;
+					}
 
-											} // this block of codes enables the user to add a new data to the database.
-											else{
+					$saveChecker = 1;
 
-												
+				} // this block of codes enables the user to add a new data to the database.
 
-												$sql = "CALL updateDATA(:userID, :firstName, :middleName, :lastName, :phoneNumber, :emailAddress)";
+				else{
+					$sql = "CALL updateDATA(:userID, :firstName, :middleName, :lastName, :phoneNumber, :emailAddress)";
 
-												try {
+					try {
 
-													$cmd = $conn->prepare($sql);
-													$cmd->bindParam("userID",$userID);
-													$cmd->bindParam("firstName", $firstName);
-													$cmd->bindParam("middleName", $middleName);
-													$cmd->bindParam("lastName", $lastName);
-													$cmd->bindParam("phoneNumber", $phoneNumber);
-													$cmd->bindParam("emailAddress", $email);
-													$cmd->execute();	
-												
-												} catch (Exception $e) {
+						$cmd = $conn->prepare($sql);
+						$cmd->bindParam("userID",$userID);
+						$cmd->bindParam("firstName", $firstName);
+						$cmd->bindParam("middleName", $middleName);
+						$cmd->bindParam("lastName", $lastName);
+						$cmd->bindParam("phoneNumber", $phoneNumber);
+						$cmd->bindParam("emailAddress", $email);
+						$cmd->execute();	
 
-													echo  $e->getMessage();
+					} catch (Exception $e) {
 
-												}
+						echo  $e->getMessage();
 
-												$_SESSION['checker'] ='';
-												$saveChecker = '';
+					}
 
-											}// this block of codes enables the user to update an existing data from the database.			
-									}
-								
-							
+					$_SESSION['checker'] ='';
+					$saveChecker = '';
 
-					?>
+				}// this block of codes enables the user to update an existing data from the database.			
+			}
+		?>
 
 				
-				<div class="mainForm">
+			<div class="mainForm">
 					
-					<div class="header">
+				<div class="header">
 						
-						<label> Phone Directory<br> Management System </label>
+					<label> Phone Directory<br> Management System </label>
 
 						<input type="text" placeholder="Type here..." class="txt-search" name="txtSearch">
 						<input type="submit" name="btnSearch" value="Search">
 
-					</div>
+				</div>
 
-					<div class="left-pnl">
+				<div class="left-pnl">
 
-						<p> Data Entry </p> <br>
+					<p> Data Entry </p> <br>
 
-						<?php
+					<?php
 
-								$fname = "";
-								$mname = "";
-								$lname = "";
-								$pnumber = "";
-								$email = "";
+						$fname = "";
+						$mname = "";
+						$lname = "";
+						$pnumber = "";
+						$email = "";
 
-							if (isset($_GET['userID'])) {
+						if (isset($_GET['userID'])) {
 
-								$_SESSION['checker'] = $_GET['checker'];
-								$_SESSION['userID'] = $_GET['userID'];
-
-
-								$sql = "SELECT * FROM users WHERE userID=:userID";
-
-								$cmd = $conn->prepare($sql);
-								$cmd->bindParam('userID',$_GET['userID']);
-								$cmd->execute();
-
-								foreach ($cmd as $row) {
-									
-								$fname = $row['firstName'];
-								$mname = $row['middleName'];
-								$lname = $row['lastName'];
-								$pnumber = $row['phoneNumber'];
-								$email = $row['emailAddress'];
-								
-								}
- 
-							} // this block of codes is needed for updating a data from the database.
+							$_SESSION['checker'] = $_GET['checker'];
+							$_SESSION['userID'] = $_GET['userID'];
 
 
+							$sql = "SELECT * FROM users WHERE userID=:userID";
 
-						?>
+							$cmd = $conn->prepare($sql);
+							$cmd->bindParam('userID',$_GET['userID']);
+							$cmd->execute();
+
+							foreach ($cmd as $row) {
+
+							$fname = $row['firstName'];
+							$mname = $row['middleName'];
+							$lname = $row['lastName'];
+							$pnumber = $row['phoneNumber'];
+							$email = $row['emailAddress'];
+
+							}
+
+						} // this block of codes is needed for updating a data from the database.
+					?>
 					
-						<input type="text" name="txtFname" placeholder="Firstname" value="<?php echo $fname?>" ><br>
-						<input type="text" name="txtMname" placeholder="Middlename" value="<?php echo $mname?>"><br>
-						<input type="text" name="txtLname" placeholder="Lastname" value="<?php echo $lname?>" ><br>
-						<input type="text" name="txtPhoneNumber" placeholder="PhoneNumber" value="<?php echo $pnumber?>" ><br>
-						<input type="text" name="txtEmail" placeholder="Email" value="<?php echo $email?>"><br>
-						<input type="submit" name="btnSave" value="Save" class="btn add">
-						
+					<input type="text" name="txtFname" placeholder="Firstname" value="<?php echo $fname?>" ><br>
+					<input type="text" name="txtMname" placeholder="Middlename" value="<?php echo $mname?>"><br>
+					<input type="text" name="txtLname" placeholder="Lastname" value="<?php echo $lname?>" ><br>
+					<input type="text" name="txtPhoneNumber" placeholder="PhoneNumber" value="<?php echo $pnumber?>" ><br>
+					<input type="text" name="txtEmail" placeholder="Email" value="<?php echo $email?>"><br>
+					<input type="submit" name="btnSave" value="Save" class="btn add">
+				</div>
 
-					</div>
+				<div class="right-pnl">
 
-					<div class="right-pnl">
-
-						<div class="table">
+					<div class="table">
 							
-							<table>
-							
+						<table>
 							<tr class="table-row">
 								<th class="col-header fullname"> Fullname </th>
 								<th class="col-header phone-number"> Phone number</th>
@@ -347,79 +334,63 @@
 								<th class="col-header buttons"> Actions </th>
 							</tr>
 
-							<?php
+						<?php
 
-								if(isset($_GET['loadData']) | isset($_GET['userID']) |  isset($_GET['delUser']) | isset($saveChecker)){
+							if(isset($_GET['loadData']) | isset($_GET['userID']) |  isset($_GET['delUser']) | isset($saveChecker)){
 
-									$sql = "CALL getData";
-									$cmd = $conn->prepare($sql);
-									$cmd->execute();
+								$sql = "CALL getData";
+								$cmd = $conn->prepare($sql);
+								$cmd->execute();
 
-									foreach ($cmd as $row) {
+								foreach ($cmd as $row) {
 
-										echo '
-
-											<tr class="table-row">
-											<th class="col-data fname">'.$row["fullname"].'</th>
-											<th class="col-data number">'.$row["phoneNumber"].'</th>
-											<th class="col-data mail">'.$row["emailAddress"].'</th>
-											<th class="col-data btns"> <a class="btn edit" href="?userID='.$row["userID"].'&checker=1">Edit</a><a class="btn del" href="?delUser='.$row["userID"].'">Delete</a></th>
-											</tr>
-
-										';
+									echo '
+										<tr class="table-row">
+										<th class="col-data fname">'.$row["fullname"].'</th>
+										<th class="col-data number">'.$row["phoneNumber"].'</th>
+										<th class="col-data mail">'.$row["emailAddress"].'</th>
+										<th class="col-data btns"> <a class="btn edit" href="?userID='.$row["userID"].'&checker=1">Edit</a><a class="btn del" href="?delUser='.$row["userID"].'">Delete</a></th>
+										</tr>
+									';
 										
-									}
+								}
+							}
 
+							if (isset($_GET['btnSearch'])){
 
+								$saveChecker ='';
+
+								$sql = 'SELECT * FROM vw_users WHERE CONCAT(fullname,phoneNumber,emailAddress) LIKE "%'.$_GET['txtSearch'].'%"';
+								$cmd = $conn->prepare($sql);
+								$cmd->execute();
+
+									
+								foreach ($cmd as $row) {
+
+									echo '
+										<tr class="table-row">
+										<th class="col-data fname">'.$row["fullname"].'</th>
+										<th class="col-data number">'.$row["phoneNumber"].'</th>
+										<th class="col-data mail">'.$row["emailAddress"].'</th>
+										<th class="col-data btns"> <a class="btn edit" href="?userID='.$row["userID"].'&checker=1">Edit</a><a class="btn del" href="?delUser='.$row["userID"].'">Delete</a></th>
+										</tr>
+									';
 								}
 
-								if (isset($_GET['btnSearch'])){
-
-									$saveChecker ='';
-
-									$sql = 'SELECT * FROM vw_users WHERE CONCAT(fullname,phoneNumber,emailAddress) LIKE "%'.$_GET['txtSearch'].'%"';
-									$cmd = $conn->prepare($sql);
-									$cmd->execute();
-
-									
-									foreach ($cmd as $row) {
-
-										echo '
-
-											<tr class="table-row">
-											<th class="col-data fname">'.$row["fullname"].'</th>
-											<th class="col-data number">'.$row["phoneNumber"].'</th>
-											<th class="col-data mail">'.$row["emailAddress"].'</th>
-											<th class="col-data btns"> <a class="btn edit" href="?userID='.$row["userID"].'&checker=1">Edit</a><a class="btn del" href="?delUser='.$row["userID"].'">Delete</a></th>
-											</tr>
-
-										';
-										
-									}
-
-								} // this block of codes enables the user to use the search engine and fetch the data from the database.
-
-									
-
-
-							?>	
-
+							} // this block of codes enables the user to use the search engine and fetch the data from the database.
+						?>	
 						</table>
 
 
-						</div>
+					</div>
 
-						<div class="footer">
-
+					<div class="footer">
 						<div class="btn-pnl">
-
-								<a class="btn add" name="btnLoad" href="?loadData=1">Load Data</a>
-
+							<a class="btn add" name="btnLoad" href="?loadData=1">Load Data</a>
 						</div>		
-
 					</div>
 						
-					</div>
+				</div>
 
 			</form>
 
